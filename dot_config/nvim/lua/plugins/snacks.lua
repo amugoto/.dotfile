4c6d867030667d2cb1cf669941a357d6194812cf
Ingon <kgb198621@gmail.com>
2026-04-15 08:28:29 +0900

    fix

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        -- 기본적으로 숨김 파일을 보여줌
+        -- hidden = true,
+        -- gitignore 된 파일도 보고 싶다면 true로 설정
+        -- ignored = true,
+        replace_netrw = true
+      },
+
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
